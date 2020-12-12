6e0202a3d3b63bf88622ef023dd791efd8acfdac
James Jorissen <jamesjorissen@gmail.com>
2020-12-12 11:17:49 -0600

    changed null to false; fixed phq9 stuff

--- models/testGAD7.js
+++ models/testGAD7.js
@@ -1,46 +1,42 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     var testGAD7 = sequelize.define("testGAD7", {
-        feelig: {
+        feeling: {
             type: DataTypes.INTEGER,
-            allowNull: true,
+            allowNull: false,
             default: 0
         },
         control: {
             type: DataTypes.INTEGER,
-            allowNull: true,
+            allowNull: false,
             default: 0
         },
         worrying: {
             type: DataTypes.INTEGER,
-            allowNull: true,
+            allowNull: false,
             default: 0
         },
         trouble: {
             type: DataTypes.INTEGER,
-            allowNull: true,
+            allowNull: false,
             default: 0
         },
         restless: {
             type: DataTypes.INTEGER,
-            allowNull: true,
+            allowNull: false,
             default: 0
         },
         annoyed: {
             type: DataTypes.INTEGER,
-            allowNull: true,
+            allowNull: false,
             default: 0
         },
-        affraid: {
+        afraid: {
             type: DataTypes.INTEGER,
-            allowNull: true,
+            allowNull: false,
             default: 0
         },
         dateofGAD7: {
             type: DataTypes.DATE,
-            allowNull: false
-        },
-        studentId: {
-            type: DataTypes.INTEGER,
             allowNull: false
         }
     });
